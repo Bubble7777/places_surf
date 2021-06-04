a119ba8bb01ec6dbae62f2a1bd7c435eb1e3766c
DamirKrg <sdmmds93@gmail.com>
2021-06-04 19:32:16 +0600

    add mocks

---
+++ lib/mocks.dart
@@ -0,0 +1,27 @@
+import 'package:places/domain/sight.dart';
+
+class Mocks {
+  final List<Sight> mocks = [
+    Sight(
+        'Эйфелева башня',
+        48.858093,
+        2.294694,
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Tour_Eiffel_Wikimedia_Commons_%28cropped%29.jpg/300px-Tour_Eiffel_Wikimedia_Commons_%28cropped%29.jpg',
+        'Металлическая башня в центре Парижа, самая узнаваемая его архитектурная достопримечательность.',
+        'Башня'),
+    Sight(
+        'Статуя Свободы',
+        40.6892776,
+        74.0445938,
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Statue_of_Liberty%2C_NY.jpg/280px-Statue_of_Liberty%2C_NY.jpg',
+        'Статуя Свободы — второй по высоте маяк страны, однако она выполняет световую навигационную роль для морских судов как свою вторичную функцию, поэтому в списки маяков страны её не включают.',
+        'Статуя'),
+    Sight(
+        'Бурдж-Халифа',
+        25.19722,
+        55.27417,
+        'https://upload.wikimedia.org/wikipedia/ru/thumb/2/2f/Burj_Khalifa_building.jpg/266px-Burj_Khalifa_building.jpg',
+        'Сверхвысотный небоскрёб высотой 828 метров в Дубае, самое высокое и самое многоэтажное здание, самое высокое сооружение, единственный 828-метровый и 163-этажный небоскрёб в мире.',
+        'Небоскрёб')
+  ];
+}
--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -12,65 +12,65 @@ class _SightListScreenState extends State<SightListScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        toolbarHeight: 112,
-        backgroundColor: Colors.white,
-        elevation: 0,
-        title:
-            // Text(
-            //   'Список \nинтересных мест',
-            //   style: TextStyle(
-            //       height: 1.12,
-            //       color: Colors.black,
-            //       fontSize: 32,
-            //       fontStyle: FontStyle.normal,
-            //       fontWeight: FontWeight.bold,
-            //       fontFamily: 'Roboto-Regular'),
-            // )
-            RichText(
-          textAlign: TextAlign.left,
-          text: TextSpan(
-              text: 'C',
-              style: TextStyle(
-                  height: 1.12,
-                  color: Colors.green,
-                  fontSize: 32,
-                  fontStyle: FontStyle.normal,
-                  fontWeight: FontWeight.w700,
-                  fontFamily: 'Roboto-Regular'),
-              children: <TextSpan>[
-                TextSpan(
-                  text: 'писок',
-                  style: TextStyle(
-                      height: 1.12,
-                      color: Colors.black,
-                      fontSize: 32,
-                      fontStyle: FontStyle.normal,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Roboto-Regular'),
-                ),
-                TextSpan(
-                  text: '\nи',
-                  style: TextStyle(
-                      height: 1.12,
-                      color: Colors.yellow,
-                      fontSize: 32,
-                      fontStyle: FontStyle.normal,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Roboto-Regular'),
-                ),
-                TextSpan(
-                  text: 'нтересных мест',
-                  style: TextStyle(
-                      height: 1.12,
-                      color: Colors.black,
-                      fontSize: 32,
-                      fontStyle: FontStyle.normal,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Roboto-Regular'),
-                ),
-              ]),
-        ),
-      ),
+          toolbarHeight: 112,
+          backgroundColor: Colors.white,
+          elevation: 0,
+          title: Text(
+            'Список \nинтересных мест',
+            style: TextStyle(
+                height: 1.12,
+                color: Colors.black,
+                fontSize: 32,
+                fontStyle: FontStyle.normal,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Roboto-Regular'),
+          )
+          // RichText(
+          //   textAlign: TextAlign.left,
+          //   text: TextSpan(
+          //       text: 'C',
+          //       style: TextStyle(
+          //           height: 1.12,
+          //           color: Colors.green,
+          //           fontSize: 32,
+          //           fontStyle: FontStyle.normal,
+          //           fontWeight: FontWeight.w700,
+          //           fontFamily: 'Roboto-Regular'),
+          //       children: <TextSpan>[
+          //         TextSpan(
+          //           text: 'писок',
+          //           style: TextStyle(
+          //               height: 1.12,
+          //               color: Colors.black,
+          //               fontSize: 32,
+          //               fontStyle: FontStyle.normal,
+          //               fontWeight: FontWeight.bold,
+          //               fontFamily: 'Roboto-Regular'),
+          //         ),
+          //         TextSpan(
+          //           text: '\nи',
+          //           style: TextStyle(
+          //               height: 1.12,
+          //               color: Colors.yellow,
+          //               fontSize: 32,
+          //               fontStyle: FontStyle.normal,
+          //               fontWeight: FontWeight.bold,
+          //               fontFamily: 'Roboto-Regular'),
+          //         ),
+          //         TextSpan(
+          //           text: 'нтересных мест',
+          //           style: TextStyle(
+          //               height: 1.12,
+          //               color: Colors.black,
+          //               fontSize: 32,
+          //               fontStyle: FontStyle.normal,
+          //               fontWeight: FontWeight.bold,
+          //               fontFamily: 'Roboto-Regular'),
+          //         ),
+          //       ]),
+          // ),
+
+          ),
       body: Center(
         child: Text("Hello!"),
       ),
