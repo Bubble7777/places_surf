8a4c91b4887fa36ea977904b046b3a3a3216eafe
DamirKrg <sdmmds93@gmail.com>
2021-06-09 22:55:43 +0600

    add loader for image/change NetworkImage to image.networ

--- lib/mocks.dart
+++ lib/mocks.dart
@@ -8,7 +8,7 @@ void add() {
         'Эйфелева башня',
         48.858093,
         2.294694,
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Tour_Eiffel_Wikimedia_Commons_%28cropped%29.jpg/300px-Tour_Eiffel_Wikimedia_Commons_%28cropped%29.jpg',
+        'https://img.fonwall.ru/o/5d/eiffel-tower-paris-france-eyfeleva-bashnya-jhqn.jpg',
         'Металлическая башня в центре Парижа, самая узнаваемая его архитектурная достопримечательность.',
         'Башня'),
   );
@@ -17,7 +17,7 @@ void add() {
         'Статуя Свободы',
         40.6892776,
         74.0445938,
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Statue_of_Liberty%2C_NY.jpg/280px-Statue_of_Liberty%2C_NY.jpg',
+        'https://i.pinimg.com/originals/48/c9/d8/48c9d87b49c260322e54c550905f5b58.jpg',
         'Статуя Свободы — второй по высоте маяк страны, однако она выполняет световую навигационную роль для морских судов как свою вторичную функцию, поэтому в списки маяков страны её не включают.',
         'Статуя'),
   );
@@ -25,7 +25,7 @@ void add() {
       'Бурдж-Халифа',
       25.19722,
       55.27417,
-      'https://upload.wikimedia.org/wikipedia/ru/thumb/2/2f/Burj_Khalifa_building.jpg/266px-Burj_Khalifa_building.jpg',
+      'https://magnitico.com/wp-admin/admin-ajax.php?action=rank_math_overlay_thumb&id=7939&type=magnitico-com',
       'Сверхвысотный небоскрёб высотой 828 метров в Дубае, самое высокое и самое многоэтажное здание, самое высокое сооружение, единственный 828-метровый и 163-этажный небоскрёб в мире.',
       'Небоскрёб'));
 }
--- lib/ui/screen/sight_card.dart
+++ lib/ui/screen/sight_card.dart
@@ -7,94 +7,110 @@ class SightCard extends StatelessWidget {
   final Sight sight;
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 3 / 2,
-      child: Column(
-        children: [
-          SizedBox(
-            height: 16,
-          ),
-          Container(
-              padding: EdgeInsets.only(top: 16.0),
-              width: 328,
-              height: 96,
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: NetworkImage(sight.url),
-                    fit: BoxFit.fitWidth,
-                  ),
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(16),
-                    topRight: Radius.circular(16),
-                  )),
-              child: Container(
-                margin: EdgeInsets.only(left: 16),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      sight.type,
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'Roboto-Regular',
-                          fontStyle: FontStyle.normal,
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    IconButton(
-                        padding: EdgeInsets.only(bottom: 20),
-                        icon: SvgPicture.asset(
-                          'res/assets/icons/heart.svg',
-                          color: Colors.white,
-                          height: 20,
-                          width: 18,
-                        ),
-                        onPressed: () {})
-                  ],
-                ),
-              )),
-          Container(
-            alignment: Alignment.topLeft,
+    return Column(
+      children: [
+        SizedBox(
+          height: 16,
+        ),
+        Container(
             width: 328,
-            child: Container(
-              margin: EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    sight.name,
-                    maxLines: 2,
+            height: 96,
+            child: Stack(
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(12.0),
+                    topRight: Radius.circular(12.0),
+                  ),
+                  child: Image.network(
+                    sight.url,
+                    width: 328,
+                    fit: BoxFit.fill,
+                    loadingBuilder: (BuildContext context, Widget child,
+                        ImageChunkEvent loadingProgress) {
+                      if (loadingProgress == null) {
+                        return child;
+                      }
+                      return Center(
+                        child: CircularProgressIndicator(
+                          value: loadingProgress.expectedTotalBytes != null
+                              ? loadingProgress.cumulativeBytesLoaded /
+                                  loadingProgress.expectedTotalBytes
+                              : null,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 16, top: 16),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        sight.type,
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'Roboto-Regular',
+                            fontStyle: FontStyle.normal,
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      IconButton(
+                          padding: EdgeInsets.only(bottom: 20),
+                          icon: SvgPicture.asset(
+                            'res/assets/icons/heart.svg',
+                            color: Colors.white,
+                            height: 20,
+                            width: 18,
+                          ),
+                          onPressed: () {})
+                    ],
+                  ),
+                ),
+              ],
+            )),
+        Container(
+          alignment: Alignment.topLeft,
+          width: 328,
+          child: Container(
+            margin: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  sight.name,
+                  maxLines: 2,
+                  style: TextStyle(
+                    fontStyle: FontStyle.normal,
+                    fontFamily: 'Roboto-Regular',
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: Color(0xFF3B3E5B),
+                  ),
+                ),
+                ConstrainedBox(
+                  constraints: BoxConstraints.expand(width: 164, height: 46),
+                  child: Text(
+                    sight.details,
                     style: TextStyle(
                       fontStyle: FontStyle.normal,
                       fontFamily: 'Roboto-Regular',
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500,
-                      color: Color(0xFF3B3E5B),
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                      color: Color(0xFF7C7E92),
                     ),
                   ),
-                  ConstrainedBox(
-                    constraints: BoxConstraints.expand(width: 164, height: 46),
-                    child: Text(
-                      sight.details,
-                      style: TextStyle(
-                        fontStyle: FontStyle.normal,
-                        fontFamily: 'Roboto-Regular',
-                        fontSize: 14,
-                        fontWeight: FontWeight.w400,
-                        color: Color(0xFF7C7E92),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 16.0,
-                  )
-                ],
-              ),
+                ),
+                SizedBox(
+                  height: 16.0,
+                )
+              ],
             ),
-          )
-        ],
-      ),
+          ),
+        )
+      ],
     );
   }
 }
